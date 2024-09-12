c3e261244b86f43e9a94e83de36dabd86f6f485a
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-09-13 00:22:18 +0300

    style the button

--- Age-calculator/src/app/age.component.ts
+++ Age-calculator/src/app/age.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ViewEncapsulation} from "@angular/core";
 
 @Component({
     standalone: true,
     selector: "age-profile",
     templateUrl: './age.component.html',
     styleUrls: ['./age.component.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export default class ageComponent {
 
