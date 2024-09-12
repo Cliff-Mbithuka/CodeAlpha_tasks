144892f7fe3708302f757d7baa53699ddbda2050
Clifford Musyoka <cliffordmusyoka1@gmail.com>
2024-09-12 11:28:53 +0300

    create the first component

---
+++ Age-calculator/src/app/age.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: "age-profile",
+    templateUrl: './age.component.html',
+    styleUrls: ['./age.component.css'],
+})
+export default class ageComponent {
+
+}
